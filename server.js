6b0e03a3a31c2be74b813369c366a761db45749d
Martina Callegaro <marticallegaro29j@gmail.com>
2025-05-06 21:03:46 -0300

    actualizacion server.js
    Corrección del server, ya que generaba un error: path To Regexp Error.

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const path = require('path');
-const apiRoutes = require('./routes/api.js');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const path = require('path')
+const apiRoutes = require('./routes/api.js')
+const app = express()
+const PORT = process.env.PORT || 3000
 
 
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Rutas API
-app.use('/api', apiRoutes);
+app.use('/api', apiRoutes)
 
 // Redirigir cualquier otra ruta al inicio
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 // Servidor levantado
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+  console.log(`Servidor corriendo en http://localhost:${PORT}`)
+})
