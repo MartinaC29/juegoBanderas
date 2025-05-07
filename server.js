0daf20f863e3bcf2d30b4a9cc6cc705a4387ebc7
Martina Callegaro <marticallegaro29j@gmail.com>
2025-05-06 23:56:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const apiRoutes = require('./routes/api');
 const preguntas = require('./controllers/preguntas-controllers');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
